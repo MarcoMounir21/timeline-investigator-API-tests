5db292b95fb8a2704e93215691685c0b815e7a4a
MarcoMounir21 <mam@ext.avian.dk>
2021-06-21 16:03:13 +0200

    Add implementation of the happy path scenario of view investigation case

---
+++ src/test/java/utils/CaseUtils.java
@@ -0,0 +1,47 @@
+package utils;
+
+import com.google.gson.JsonObject;
+import helpers.BackendInvoker;
+import helpers.ElasticsearchInvoker;
+import io.cucumber.datatable.DataTable;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import java.util.Map;
+
+public class CaseUtils {
+  private static final String URI = "cases";
+  private static final String BACK_URI = "case";
+  private static Response response;
+
+  public static void createCase(DataTable dataTable) {
+    Map<String, String> caseData = dataTable.asMaps().get(0);
+    JsonObject caseRequestBody = CaseUtils.createPostCaseElasticsearchRequestBody(caseData);
+    ElasticsearchInvoker.deleteIndex(URI);
+    ElasticsearchInvoker.createIndex(URI);
+    ElasticsearchInvoker.createDocument(URI, caseRequestBody, caseData.get("Id"));
+  }
+
+  public static JsonObject createPostCaseElasticsearchRequestBody(Map<String, String> caseData) {
+    JsonObject investigationCase = new JsonObject();
+    investigationCase.addProperty("Name", caseData.get("Name"));
+    investigationCase.addProperty("Created_at", caseData.get("CreatedAt"));
+    investigationCase.addProperty("Description", caseData.get("Description"));
+    investigationCase.addProperty("From", caseData.get("From"));
+    investigationCase.addProperty("To", caseData.get("To"));
+    return investigationCase;
+  }
+
+  public static void getCaseBackend(Integer id) throws InterruptedException {
+    CaseUtils.response = BackendInvoker.sendGetRequest(BACK_URI, id.toString());
+  }
+
+  public static void assertViewCase(DataTable responseTable) {
+    Map<String, String> expectedCaseMap = responseTable.asMaps().get(0);
+    JsonObject jsonResponse = CaseUtils.response.then().extract().as(JsonObject.class);
+    JsonObject caseResponse = jsonResponse.get("case").getAsJsonObject();
+    Assert.assertEquals(expectedCaseMap.get("Name"), caseResponse.get("name").getAsString());
+    Assert.assertEquals(
+        expectedCaseMap.get("Description"), caseResponse.get("description").getAsString());
+  }
+}
---
+++ src/test/resources/features/case_view.feature
@@ -0,0 +1,13 @@
+Feature: View Investigation Case
+
+  Background:
+    Given insert the following investigation cases:
+      | Id | Name       | CreatedAt  | Description            | From       | To         |
+      | 1  | First Case | 01-06-2021 | This is the first case | 01-06-2021 | 01-07-2021 |
+
+  Scenario: View Investigation Case As a Super User
+    Given user is logged in as a super user
+    When user views an investigation case with id 1
+    Then an investigation case is returned to the user with the following data:
+      | Name       | CreatedAt  | Description            | From       | To         |
+      | First Case | 01-06-2021 | This is the first case | 01-06-2021 | 01-07-2021 |
